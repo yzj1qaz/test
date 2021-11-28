819998002276619abf0d1a7504055cdb4f7239ec
yzj1qaz <15133025416@163.com>
2021-11-28 15:30:07 +0800

    ‘添加

--- tests.py
+++ tests.py
@@ -23,28 +23,23 @@
 
 
 # for i in gg:
-#     dict[i[0]] = i[1]
 #
 #
-# print(dict)
 
 
 # lst = [3,2,7,5,4,9,8]
 #
-# for i in range(len(lst)):
 #
 #     for j in range(len(lst)-i-1):
 #
-#         if lst[j] > lst[j+1]:
 #
 #             lst[j],lst[j+1] =lst[j+1],lst[j]
-# print('lst:',lst)
 
 
 
 
-input = 'I Love Python'
-output= 'I evoL nohtyP'
+input = 'I Loves Python'
+output= 'I evoLs nohtyP'
 
 
 input = input[::-1]
