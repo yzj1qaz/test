025d3435243a63b465e7f1e5534bcfa1f1fa5617
yzj1qaz <15133025416@163.com>
2021-11-28 15:32:49 +0800

    ’teststrt

--- tests.py
+++ tests.py
@@ -2,38 +2,8 @@
 
 # dd = "aabcdbefgde"
 # strs = ''
-# for i in dd:
-#     print('i:',i)
-#     coun = dd.count(i)
-#     print('coun: ',coun)
-#     if coun==1:
-#         strs = strs+i
-#
-#
-# print(strs)    #cdbefg
 
 
-# dic = {'c':1, 'b':2, 't':3}
-#
-#
-# gg = sorted(dic.items(), key=lambda d:d[0],reverse=True)
-# print(gg)
-# print(dic)
-# dict = {}
-
-
-# for i in gg:
-#
-#
-
-
-# lst = [3,2,7,5,4,9,8]
-#
-#
-#     for j in range(len(lst)-i-1):
-#
-#
-#             lst[j],lst[j+1] =lst[j+1],lst[j]
 
 
 
@@ -43,13 +13,6 @@ output= 'I evoLs nohtyP'
 
 
 input = input[::-1]
-
-# strs = input.split()
-#
-# lis = [i[::-1] for i in strs]
-#
-#
-# strs = " ".join(lis)
 
 print(input)
 
